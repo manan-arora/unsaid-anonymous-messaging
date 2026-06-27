2c7992f4b088ef627f07ea6f6a373259b93bd825
Manan Arora <mananarora1350@gmail.com>
2026-06-27 20:17:16 +0530

    write auth, username uniqueness check, and OTP verification logics

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -3,22 +3,33 @@ import VerificationEmail from "../../emails/VerificationEmail";
 import { ApiResponse } from "@/types/ApiResponse";
 
 export async function sendVerificationEmail(
-   email: string,
-   username: string,
-   verifyCode: string, 
-): Promise<ApiResponse>{
-    try {
-
-        const { data, error } = await resend.emails.send({
-      from: 'onboarding@resend.dev',
+  email: string,
+  username: string,
+  verifyCode: string,
+): Promise<ApiResponse> {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "onboarding@resend.dev",
       to: email,
-      subject: 'Unsaid Messaging | Verification code',
-      react: VerificationEmail({username, otp: verifyCode}),
+      subject: "Unsaid Messaging | Verification code",
+      react: VerificationEmail({ username, otp: verifyCode }),
     });
 
-       return {success: true, message: 'Verification email sent successfully'} 
-    } catch (emailError) {
-        console.error("Error sending verification email", emailError)
-        return {success: false, message: 'Failed to send verification email'}
+    console.log("Resend response:", { data, error });
+
+    if (error) {
+      return {
+        success: false,
+        message: error.message,
+      };
     }
+
+    return {
+      success: true,
+      message: "Verification email sent successfully",
+    };
+  } catch (emailError) {
+    console.error("Error sending verification email", emailError);
+    return { success: false, message: "Failed to send verification email" };
+  }
 }
--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,3 +1,5 @@
 import { Resend } from 'resend';
 
+console.log("RESEND =", process.env.RESEND_API_KEY);
+
 export const resend = new Resend(process.env.RESEND_API_KEY);
